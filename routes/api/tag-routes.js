ff6f22b1c2e8b935f561b82256d454ff067577ef
Paul Pinard <paul.k.pinard@gmail.com>
2022-12-08 13:36:41 -0500

    GET route to view a single tag

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -12,9 +12,13 @@ router.get('/', async (req, res) => {
     }
 });
 
-router.get('/:id', (req, res) => {
-  // find a single tag by its `id`
-  // be sure to include its associated Product data
+router.get('/:id', async (req, res) => {
+  try {
+    const tagData = await Tag.findByPk(req.params.id, { include: { model: Product }});
+    tagData ? res.status(200).json(tagData) : res.status(404).json({})
+} catch (err) {
+    res.status(500).json(err);
+}
 });
 
 router.post('/', (req, res) => {
