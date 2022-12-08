874d57f3d8ac071c6288d8656d2bf153f1c34e9c
Paul Pinard <paul.k.pinard@gmail.com>
2022-12-08 11:50:36 -0500

    GET route to view all products

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,9 +4,13 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
-  // find all products
-  // be sure to include its associated Category and Tag data
+router.get('/', async (req, res) => {
+    try {
+        const productsData = await Product.findAll();
+        res.status(200).json(productsData);
+    } catch (err) {
+        res.status(500).json(err)
+    }
 });
 
 // get one product
