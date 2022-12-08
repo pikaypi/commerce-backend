8b30d3dcdfba27e00c5e61c47b0a3693e5ebc776
Paul Pinard <paul.k.pinard@gmail.com>
2022-12-08 11:46:34 -0500

    POST route to add a new category

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -21,8 +21,13 @@ router.get('/:id', async (req, res) => {
     }
 });
 
-router.post('/', (req, res) => {
-  // create a new category
+router.post('/', async (req, res) => {
+    try {
+        const newCategoryData = await Category.create(req.body);
+        res.status(200).json(newCategoryData)
+    } catch (err) {
+        res.status(500).json(err)
+    }
 });
 
 router.put('/:id', (req, res) => {
