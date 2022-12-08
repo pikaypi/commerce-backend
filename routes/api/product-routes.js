23653863f82601dbd4b9027075064ddfbc349011
Paul Pinard <paul.k.pinard@gmail.com>
2022-12-08 12:04:52 -0500

    DELETE route to delete a product

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -97,8 +97,13 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
-  // delete one product by its `id` value
+router.delete('/:id', async (req, res) => {
+    try {
+        const deleteProductData = await Product.destroy({ where: { id: req.params.id }});
+        deleteProductData ? res.status(200).json(deleteProductData) : res.status(404).json(0)
+    } catch (err) {
+        res.status(500).json(err)
+    }
 });
 
 module.exports = router;
