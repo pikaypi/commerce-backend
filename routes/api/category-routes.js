2dfcb8cbfd60d0ecedbdf83123294f5e82f96537
Paul Pinard <paul.k.pinard@gmail.com>
2022-12-08 11:46:10 -0500

    GET route for a single category

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -12,9 +12,13 @@ router.get('/', async (req, res) => {
     }
 });
 
-router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+router.get('/:id', async (req, res) => {
+    try {
+        const categoryData = await Category.findByPk(req.params.id);
+        categoryData ? res.status(200).json(categoryData) : res.status(404).json({})
+    } catch (err) {
+        res.status(500).json(err);
+    }
 });
 
 router.post('/', (req, res) => {
