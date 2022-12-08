84854faea78c15af1ec358f9ba550b95ac3cddbc
Paul Pinard <paul.k.pinard@gmail.com>
2022-12-08 13:37:17 -0500

    PUT route to update a tag name

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -30,8 +30,13 @@ router.post('/', async (req, res) => {
 }
 });
 
-router.put('/:id', (req, res) => {
-  // update a tag's name by its `id` value
+router.put('/:id', async (req, res) => {
+  try {
+    const updateTagData = await Tag.update(req.body, { where: { id: req.params.id }});
+    res.status(200).json(updateTagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.delete('/:id', (req, res) => {
